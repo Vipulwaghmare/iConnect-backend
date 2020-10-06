250148f68d6935b256c5fa51775f1bf07b52a7a9
unknown <vipulwaghmare222@gmail.com>
2020-10-06 23:02:42 +0530

    validation completed

--- Controller/company.js
+++ Controller/company.js
@@ -1,6 +1,13 @@
 const Company = require("../Models/company")
+const { validationResult } = require('express-validator');
 
 exports.addCompany = (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            error: errors.array()[0].msg
+        })
+    }
 
     const company = new Company(req.body)
 
--- Routes/company.js
+++ Routes/company.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const { check } = require('express-validator');
+const Company = require('../Models/company')
 
 const { addCompany } = require('../Controller/company')
 
 router.post('/addCompany',[
-    
+    check('name')
+        .custom(value=>{
+            return Company.findOne({name: value}).then(co => {
+                if(co){
+                    return Promise.reject('Name already in use')
+                }
+            })
+        }),
+    check('email')
+        .isEmail()
+        .withMessage("Should be a valid Email")
+        .custom(value=>{
+            return Company.findOne({email: value}).then(co => {
+                if(co){
+                    return Promise.reject('E-mail already in use')
+                }
+            })
+        })
 ],addCompany)
 
 module.exports = router
