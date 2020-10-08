2c4f3dbc716dc19bcbab733b415f7982cd0b516a
unknown <vipulwaghmare222@gmail.com>
2020-10-08 17:21:13 +0530

    final

--- Routes/company.js
+++ Routes/company.js
@@ -2,19 +2,27 @@ const express = require('express')
 const router = express.Router()
 const { addCompany, getCompanyById, getCompanyByName ,editCompany, deleteCompany, getAllCompanies, getAllAscCompanies, getAllDscCompanies, getLogo } = require('../Controller/company')
 
-router.param("companyId", getCompanyById)
+// Param
+router.param("companyId", getCompanyById) 
 
+// To create a new company
 router.post('/addCompany', addCompany)
 
+// For search
 router.post("/getCompanyByName" , getCompanyByName)
 
+// TO edit a company
 router.put('/editCompany/:companyId',editCompany)
 
+// Delete a company
 router.delete('/deleteCompany/:companyId', deleteCompany)
 
-router.get('/getAllCompanies', getAllCompanies)
+// Get all companies in ascending and descending order as per need
+router.get('/getAllCompanies', getAllCompanies) 
 router.get('/getAllAscCompanies', getAllAscCompanies)
 router.get('/getAllDscCompanies', getAllDscCompanies)
+
+// middleware to get logo
 router.get('/logo/:companyId', getLogo)
 
 module.exports = router
